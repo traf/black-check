5999d3c6977eb8773aea65026789024345684064
yungwknd <yungwkndllc@gmail.com>
2025-08-11 22:13:39 -0700

    add supported chains
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- app/components/PrivyProvider.tsx
+++ app/components/PrivyProvider.tsx
@@ -1,20 +1,32 @@
-'use client';
+"use client";
 
-import { PrivyProvider } from '@privy-io/react-auth';
+import { PrivyProvider } from "@privy-io/react-auth";
+import { mainnet, sepolia } from "viem/chains";
 
-export default function PrivyWrapper({ children }: { children: React.ReactNode }) {
+export default function PrivyWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
       config={{
         appearance: {
-          theme: 'dark',
-          accentColor: '#ffffff',
+          theme: "dark",
+          accentColor: "#ffffff",
         },
-        loginMethods: ['wallet']
+        loginMethods: ["wallet"],
+        defaultChain:
+          process.env.NEXT_PUBLIC_NETWORK === "sepolia"
+            ? sepolia
+            : process.env.NEXT_PUBLIC_NETWORK === "mainnet"
+            ? mainnet
+            : sepolia,
+        supportedChains: [sepolia, mainnet],
       }}
     >
       {children}
     </PrivyProvider>
   );
-} 
+}
